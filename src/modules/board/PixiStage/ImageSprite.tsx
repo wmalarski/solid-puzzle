fea638a92b935aa521485d11253c231165681f8f
Wojciech Malarski <w.malarski@gmail.com>
2023-05-24 21:13:53 +0200

    generate random points added

--- src/modules/board/PixiStage/ImageSprite.tsx
+++ src/modules/board/PixiStage/ImageSprite.tsx
@@ -8,6 +8,119 @@ import {
 } from "solid-js";
 import { usePixiApp } from "./PixiApp";
 
+type GenerateRandomInRangeArgs = {
+  center: number;
+  max: number;
+  radius: number;
+};
+
+const generateRandomInRange = ({
+  center,
+  max,
+  radius,
+}: GenerateRandomInRangeArgs) => {
+  const random = 2 * (Math.random() - 0.5) * radius;
+  return Math.max(Math.min(center + random, max), 0);
+};
+
+type GenerateRandomRowPointsArgs = {
+  columns: number;
+  height: number;
+  rowIndex: number;
+  rows: number;
+  width: number;
+};
+
+const generateRandomRowPoints = ({
+  columns,
+  height,
+  rowIndex,
+  rows,
+  width,
+}: GenerateRandomRowPointsArgs) => {
+  const columnWidth = width / columns;
+  const rowHeight = height / rows;
+
+  const xRadius = columnWidth / 3;
+  const yRadius = rowHeight / 3;
+
+  const isBorderRow = rowIndex === 0 || rowIndex === rows;
+
+  return Array(columns + 1)
+    .fill(0)
+    .map((_value, columnIndex) => {
+      const isBorderColumn = columnIndex === 0 || columnIndex === columns;
+      const xCenter = columnIndex * columnWidth;
+      const yCenter = rowIndex * rowHeight;
+
+      return {
+        x: isBorderColumn
+          ? xCenter
+          : generateRandomInRange({
+              center: xCenter,
+              max: width,
+              radius: xRadius,
+            }),
+        y: isBorderRow
+          ? yCenter
+          : generateRandomInRange({
+              center: yCenter,
+              max: height,
+              radius: yRadius,
+            }),
+      };
+    });
+};
+
+type GenerateRandomRowsPointsArgs = {
+  columns: number;
+  height: number;
+  rows: number;
+  width: number;
+};
+
+const generateRandomRowsPoints = ({
+  columns,
+  height,
+  rows,
+  width,
+}: GenerateRandomRowsPointsArgs) => {
+  return Array(rows + 1)
+    .fill(0)
+    .map((_value, rowIndex) =>
+      generateRandomRowPoints({
+        columns,
+        height,
+        rowIndex,
+        rows,
+        width,
+      })
+    );
+};
+
+type GenerateCurvesArgs = {
+  columns: number;
+  height: number;
+  rows: number;
+  width: number;
+};
+
+const generateCurves = ({
+  columns,
+  height,
+  rows,
+  width,
+}: GenerateCurvesArgs) => {
+  const points = generateRandomRowsPoints({
+    columns,
+    height,
+    rows,
+    width,
+  });
+
+  console.log({ points });
+};
+
 type Props = {
   path: string;
 };
@@ -26,6 +139,13 @@ export const ImageSprite: Component<Props> = (props) => {
     if (!sprite) {
       return;
     }
+
+    generateCurves({
+      columns: 8,
+      height: sprite.height,
+      rows: 5,
+      width: sprite.width,
+    });
 
     const mask = new PIXI.Graphics();
     mask.beginFill(0x00ff00);
