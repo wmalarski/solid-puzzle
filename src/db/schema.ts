d89657e0ce63bc11425b5b3514770e20c286c363
Wojciech Malarski <w.malarski@gmail.com>
2023-07-23 13:43:32 +0200

    schema changes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,11 @@
-import { int, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import {
+  int,
+  integer,
+  numeric,
+  sqliteTable,
+  text,
+} from "drizzle-orm/sqlite-core";
 
 export const user = sqliteTable("auth_user", {
   id: text("id").primaryKey(),
@@ -28,17 +35,14 @@ export const key = sqliteTable("auth_key", {
 
 export const board = sqliteTable("game_room", {
   config: text("config").notNull(),
+  createdAt: numeric("created_at").default(sql`(CURRENT_TIMESTAMP)`),
   id: text("id").primaryKey(),
   media: text("media").notNull(),
   name: text("name").notNull(),
   ownerId: text("owner_id")
     .notNull()
     .references(() => user.id),
-});
-
-export const space = sqliteTable("space", {
-  key: text("id").primaryKey().notNull(),
-  version: integer("version"),
+  updatedAt: numeric("updated_at").default(sql`(CURRENT_TIMESTAMP)`),
 });
 
 // We will store our chat messages within "spaces".
@@ -46,39 +50,29 @@ export const space = sqliteTable("space", {
 // Note that in many applications there is already some domain object that
 // already fills the role of a "space". In that case, that table can double
 // as the space table.
-await t.none(`create table space (
-        key text not null unique primary key,
-        version integer)`);
-await t.none(
-  `insert into space (key, version) values ('${defaultSpaceID}', 0)`
-);
+export const space = sqliteTable("space", {
+  key: text("id").primaryKey().notNull(),
+  version: integer("version"),
+});
+
+// await t.none(`create table space (
+//         key text not null unique primary key,
+//         version integer)`);
+// await t.none(
+//   `insert into space (key, version) values ('${defaultSpaceID}', 0)`
+// );
 
 // Stores chat messages.
-await t.none(`create table message (
-      id text primary key not null,
-      space_id text not null references space(key),
-      sender varchar(255) not null,
-      content text not null,
-      ord integer not null,
-      deleted boolean not null,
-      version integer not null)`);
-
 export const message = sqliteTable("message", {
+  content: text("content").notNull(),
+  deleted: int("deleted").notNull(),
   id: text("id").primaryKey().notNull(),
+  ord: integer("ord").notNull(),
+  sender: text("sender", { length: 255 }).notNull(),
   space_id: text("space_id")
     .notNull()
     .references(() => space.key),
-  sender: text("sender", { length: 255 }).notNull(),
-  content: text("content").notNull(),
-  ord: integer("ord").notNull(),
-  deleted: int("deleted").notNull(),
   version: integer("version").notNull(),
-
-  media: text("media").notNull(),
-  name: text("name").notNull(),
-  ownerId: text("owner_id")
-    .notNull()
-    .references(() => user.id),
 });
 
 // Stores last mutationID processed for each Replicache client.
