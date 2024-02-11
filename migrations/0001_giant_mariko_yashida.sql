aef597c9780b6e95b3adf905b475abc3df77c1b7
Wojciech Malarski <w.malarski@gmail.com>
2024-02-11 15:03:33 +0100

    removes old migration files

--- migrations/0001_giant_mariko_yashida.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE auth_session ADD `expires_at` integer NOT NULL;--> statement-breakpoint
-ALTER TABLE `auth_session` DROP COLUMN `active_expires`;--> statement-breakpoint
-ALTER TABLE `auth_session` DROP COLUMN `idle_expires`;--> statement-breakpoint
-ALTER TABLE `auth_user` DROP COLUMN `last_names`;--> statement-breakpoint
-ALTER TABLE `auth_user` DROP COLUMN `names`;
