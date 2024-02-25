5d6fa61e0ed83060f57686d03a197d9dc92038b9
Wojciech Malarski <w.malarski@gmail.com>
2024-02-25 19:46:26 +0100

    adds cloudflare setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "@solidjs/start/config";
 
+// https://ryanjc.com/blog/solidstart-cloudflare-pages/
 export default defineConfig({
   start: {
-    middleware: "./src/middleware.ts"
+    middleware: "./src/middleware.ts",
+    server: {
+      preset: "cloudflare-pages",
+      // We will need to enable CF Pages node compatiblity
+      // https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
+      rollupConfig: {
+        external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"]
+      }
+    }
   },
   ssr: {
     noExternal: ["@kobalte/core"]
